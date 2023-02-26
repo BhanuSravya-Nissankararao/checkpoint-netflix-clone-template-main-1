1f2cedabb10df4dd2adf448a78700a4de3edfe7c
BhanuSravya-Nissankararao <121043749+BhanuSravya-Nissankararao@users.noreply.github.com>
2023-02-26 12:48:01 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 function App(){
     return(
-        <>
-        <h1>Netflix Clone</h1>
-        </>
+        // <>
+        // <h1>Netflix Clone</h1>
+        // </>
+        <div className="App">
+          <Navbar />
+          {/* <Banner /> */}
+        </div>
     );
 }
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,14 @@ import "./Navbar.css";
 const Navbar = () => {
   const [show, setShow] = useState(false);
   useEffect(() => {
-
+    
   }, []);
 
   return (
-   
+    <nav className="navbar" style={{ show }}>
+      <img src={`${process.env.PUBLIC_URL}/netflix-logo.png`} className="nav__logo" />
+      <img src={`${process.env.PUBLIC_URL}/netflix-avatar.png`}  className="nav__avatar" />
+    </nav>
   );
 };
 
